f077f2c7eef1b5ece6f987e769c641cdbe819f09
gaurav00008 <gauravpaneru95@gmail.com>
2026-03-06 21:17:12 +0545

    Null

---
+++ revision.js
@@ -0,0 +1,22 @@
+const varId=7
+
+accountEmail = "gaurav12@gmail.com"
+accountPass = "22883883"
+accountCity = "Delhi"
+
+console.log(varId);
+
+
+console.table([accountEmail,accountPass,accountCity])
+
+
+
+// DATATYPES IN JAVASCRIPT
+let name = "Gaurav" // String
+let age = 22 // Number
+let isStudent = true // Boolean
+let hobbies = ["coding", "gaming", "traveling"] // Array 
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
