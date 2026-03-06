438c70e1501308a333824fb7469d5d0039418a1d
gaurav00008 <gauravpaneru95@gmail.com>
2026-03-06 21:48:52 +0545

    Operators

--- revision.js
+++ revision.js
@@ -1,22 +1,50 @@
-const varId=7
+// const varId=7
 
-accountEmail = "gaurav12@gmail.com"
-accountPass = "22883883"
-accountCity = "Delhi"
+// accountEmail = "gaurav12@gmail.com"
+// accountPass = "22883883"
+// accountCity = "Delhi"
 
-console.log(varId);
-
-
-console.table([accountEmail,accountPass,accountCity])
+// console.log(varId);
 
 
-
-// DATATYPES IN JAVASCRIPT
-let name = "Gaurav" // String
-let age = 22 // Number
-let isStudent = true // Boolean
-let hobbies = ["coding", "gaming", "traveling"] // Array 
+// console.table([accountEmail,accountPass,accountCity])
 
 
-console.log(typeof undefined); //undefined
-console.log(typeof null); //object
+
+// // DATATYPES IN JAVASCRIPT
+// let name = "Gaurav" // String
+// let age = 22 // Number
+// let isStudent = true // Boolean
+// let hobbies = ["coding", "gaming", "traveling"] // Array 
+
+
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
+
+
+
+
+// //OPERATION in JAVASCRIPT
+// console.log(5 + 3); // Addition
+// console.log(5 - 3); // Subtraction
+// console.log(5 * 3); // Multiplication
+// console.log(5 / 3); // Division
+// console.log(5 % 3); // Modulus
+// console.log(5 ** 3); // Exponentiation
+
+
+
+// let str1 = "Hello"
+// let str2 = "World"
+
+// console.log(str1 + " " + str2); // Concatenation
+// console.log(str1.length); // Length of the string
+
+
+
+// // COMAPRISON OPERATORS
+// console.log(5 == 5);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
