2ffc157a892edf550c9d211a122d29e2ebe51723
gaurav00008 <gauravpaneru95@gmail.com>
2026-03-07 21:55:56 +0545

    MATHS IN JAVASCRIPT

--- revision.js
+++ revision.js
@@ -1,124 +1,168 @@
-// const varId=7
+// // // const varId=7
 
-// accountEmail = "gaurav12@gmail.com"
-// accountPass = "22883883"
-// accountCity = "Delhi"
+// // // accountEmail = "gaurav12@gmail.com"
+// // // accountPass = "22883883"
+// // // accountCity = "Delhi"
 
-// console.log(varId);
-
-
-// console.table([accountEmail,accountPass,accountCity])
+// // // console.log(varId);
 
 
-
-// // DATATYPES IN JAVASCRIPT
-// let name = "Gaurav" // String
-// let age = 22 // Number
-// let isStudent = true // Boolean
-// let hobbies = ["coding", "gaming", "traveling"] // Array 
-
-
-// console.log(typeof undefined); //undefined
-// console.log(typeof null); //object
+// // // console.table([accountEmail,accountPass,accountCity])
 
 
 
-
-// //OPERATION in JAVASCRIPT
-// console.log(5 + 3); // Addition
-// console.log(5 - 3); // Subtraction
-// console.log(5 * 3); // Multiplication
-// console.log(5 / 3); // Division
-// console.log(5 % 3); // Modulus
-// console.log(5 ** 3); // Exponentiation
+// // // // DATATYPES IN JAVASCRIPT
+// // // let name = "Gaurav" // String
+// // // let age = 22 // Number
+// // // let isStudent = true // Boolean
+// // // let hobbies = ["coding", "gaming", "traveling"] // Array 
 
 
-
-// let str1 = "Hello"
-// let str2 = "World"
-
-// console.log(str1 + " " + str2); // Concatenation
-// console.log(str1.length); // Length of the string
-
-
-
-// // COMAPRISON OPERATORS
-// console.log(5 == 5);
-
-// console.log(null>0);
-// console.log(null==0);
-// console.log(null>=0);
-
-
-// //OBJEST IN JAVASCRIPT
-// const heroes = ["Superman", "Batman", "Spiderman"];
-// let myObj={
-//     name:"Gaurav",     
-//     age:22,
-// }
-
-
-
-//Stack Memory and Heap Memory || Primitives and Non-Primitives
-
-// Primitives are stored in Stack Memory
-// Non-Primitives are stored in Heap Memory
-
-// let myYTChannel = "CodeWithGaurav" 
-
-// let anothername= myYTChannel
-// anothername = "CodeWithGaurav2"
-
-// console.log(myYTChannel); // CodeWithGaurav
-// console.log(anothername); // CodeWithGaurav2
+// // // console.log(typeof undefined); //undefined
+// // // console.log(typeof null); //object
 
 
 
 
-
-/*STRINGS IN JAVASCRIPT*/
-const name = "Alogmi"
-const city = "Kathmandu"
-
-// Concatenation
-// console.log(name + " " + city) /*Outdated technique*/
-
-// Template Literals
-console.log(`${name} ${city}`) /*Modern technique*/
+// // // //OPERATION in JAVASCRIPT
+// // // console.log(5 + 3); // Addition
+// // // console.log(5 - 3); // Subtraction
+// // // console.log(5 * 3); // Multiplication
+// // // console.log(5 / 3); // Division
+// // // console.log(5 % 3); // Modulus
+// // // console.log(5 ** 3); // Exponentiation
 
 
 
-const gameName = new String("BULBASAUR_IS_A_POKEMON")  //TO DECLARE A STRING OBJECT
-console.log(gameName); 
-console.log(typeof gameName); //object
+// // // let str1 = "Hello"
+// // // let str2 = "World"
+
+// // // console.log(str1 + " " + str2); // Concatenation
+// // // console.log(str1.length); // Length of the string
 
 
 
-console.log(gameName.length); // Length of the string
-console.log(gameName.toUpperCase()); // Convert to uppercase
-console.log(gameName.toLowerCase()); // Convert to lowercase
+// // // // COMAPRISON OPERATORS
+// // // console.log(5 == 5);
+
+// // // console.log(null>0);
+// // // console.log(null==0);
+// // // console.log(null>=0);
 
 
-console.log(gameName.charAt(2)); // Get character at index 2
-console.log(gameName.indexOf("this")); // Get index of the first occurrence of "this"
+// // // //OBJEST IN JAVASCRIPT
+// // // const heroes = ["Superman", "Batman", "Spiderman"];
+// // // let myObj={
+// // //     name:"Gaurav",     
+// // //     age:22,
+// // // }
 
 
-const newString = gameName.substring(0, 3); // Get substring from index 0 to 4
-console.log(newString); // Output: "Hey "
+
+// // //Stack Memory and Heap Memory || Primitives and Non-Primitives
+
+// // // Primitives are stored in Stack Memory
+// // // Non-Primitives are stored in Heap Memory
+
+// // // let myYTChannel = "CodeWithGaurav" 
+
+// // // let anothername= myYTChannel
+// // // anothername = "CodeWithGaurav2"
+
+// // // console.log(myYTChannel); // CodeWithGaurav
+// // // console.log(anothername); // CodeWithGaurav2
 
 
 
-const str1= gameName.slice(-5, 4); // Get substring from index 0 to 4
-console.log(str1); // Output: "Hey"
 
 
-const trimString = "   Hello World!   "
-console.log(trimString); // Original string with whitespace
+// // /*STRINGS IN JAVASCRIPT*/
+// // const name = "Alogmi"
+// // const city = "Kathmandu"
 
-console.log(trimString.trim()); // Remove whitespace from both ends
+// // // Concatenation
+// // // console.log(name + " " + city) /*Outdated technique*/
 
-const url = "https://www.codewithgaura%200.com"
+// // // Template Literals
+// // console.log(`${name} ${city}`) /*Modern technique*/
 
-console.log(url.replace('%200', 'v')); // Replace %20 with space
 
-console.log(url.split('_')); // Split the string into an array using '.' as the separator
+
+// // const gameName = new String("BULBASAUR_IS_A_POKEMON")  //TO DECLARE A STRING OBJECT
+// // console.log(gameName); 
+// // console.log(typeof gameName); //object
+
+
+
+// // console.log(gameName.length); // Length of the string
+// // console.log(gameName.toUpperCase()); // Convert to uppercase
+// // console.log(gameName.toLowerCase()); // Convert to lowercase
+
+
+// // console.log(gameName.charAt(2)); // Get character at index 2
+// // console.log(gameName.indexOf("this")); // Get index of the first occurrence of "this"
+
+
+// // const newString = gameName.substring(0, 3); // Get substring from index 0 to 4
+// // console.log(newString); // Output: "Hey "
+
+
+
+// // const str1= gameName.slice(-5, 4); // Get substring from index 0 to 4
+// // console.log(str1); // Output: "Hey"
+
+
+// // const trimString = "   Hello World!   "
+// // console.log(trimString); // Original string with whitespace
+
+// // console.log(trimString.trim()); // Remove whitespace from both ends
+
+// // const url = "https://www.codewithgaura%200.com"
+
+// // console.log(url.replace('%200', 'v')); // Replace %20 with space
+
+// // console.log(url.split('_')); // Split the string into an array using '.' as the separator
+
+
+
+// /*NUMBERS AND MATHS IN JAVASCRIPTS*/ 
+
+// //NUMBERS IN JAVASCRIPT
+// // const num=100;
+// // console.log(num);//NUMBER
+
+// // // console.log(typeof balance); //object  
+
+
+// // const balance = new Number(500.1263845); //TO DECLARE A NUMBER OBJECT
+// // console.log(balance);
+
+
+
+// // console.log(balance.toString().length); // Convert number to string)
+// // console.log(balance.toFixed(2)); // Format number to 2 decimal places
+
+// // const otherNumber = 3.14159;
+// // console.log(otherNumber.toPrecision(3)); // Format number to 3 significant digits
+
+
+// // const hundreds = 100000000
+// // console.log(hundreds.toLocaleString('en-US')); // Format number with commas as thousands separators 
+
+
+// // MATHS IN JAVASCRIPT
+// console.log(Math);
+// console.log(Math.PI); // Get the value of PI 
+// console.log(Math.abs(-4)); // Get the absolute value of -4
+// console.log(Math.round(8.31)); // Round to the nearest integer
+
+// console.log(Math.min(4,5,6,7,8,9,10,119,182,128)); // Get the minimum value from a list of numbers
+// console.log(Math.max(4,5,6,7,8,9,10,119,182,128)); // Get the maximum value from a list of numbers
+
+
+console.log(Math.random()); // Get a random number between 0 and 1
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
